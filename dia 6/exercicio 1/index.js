48d050b2d37532646925564fffd34186b7810368
Thiago Henrique <thitrabuco06@gmail.com>
2024-05-09 22:06:13 -0300

    Aprendendo FOR

---
+++ dia 6/exercicio 1/index.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Insira um numero para fazer a contagem"))
+
+for(let i = 0; i <= numero; i++){
+    console.log(i)
+}
+
+for(let i = 0; i <= 50; i += 5) {
+    console.log(i)
+}
+
+for(let i = 50; i >= 0 ; i -= 5) {
+    console.log(i)
+}
