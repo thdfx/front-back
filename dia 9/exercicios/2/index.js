e721874ef27332f7bb73b437889c040ee6c6f832
Thiago Henrique <thitrabuco06@gmail.com>
2024-05-22 16:57:03 -0300

    exercicios de switch case e loop

---
+++ dia 9/exercicios/2/index.js
@@ -0,0 +1,21 @@
+//Variaveis e loop
+let confirmar = "n";
+let nome = prompt("Digite o seu nome");
+let idade = parseInt(prompt("Digite sua idade"));
+let salario = parseFloat(prompt("Digite seu salario atual"));
+while (confirmar != "s") {
+    console.log("Nome: " + nome + ", idade: " + idade + ", salario atual:" + salario);
+    confirmar = prompt("As informações estão corretas? (s) ou (n)");
+}
+
+
+//Previsao do salario
+let aumento = 0.015
+console.log("Previsao salarial dos próximos 10 anos");
+
+for(let ano = 1; ano <= 10; ano++){
+    salario += salario * aumento
+    aumento *= 2
+
+    console.log((2023 + ano) + " = R$ " + salario)
+}
